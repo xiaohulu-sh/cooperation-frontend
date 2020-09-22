edccb22781f2bd5c38b3cf780cf9769a7bb8e7ae
Ralph <ralph_lu@qq.com>
2020-09-22 19:05:22 +0800

    添加NProgress

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import NProgress from 'nprogress'
+import '@/styles/nprogress.css'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
+
+let progressBarTimeout = null
+function startProgressBar() {
+  clearTimeout(progressBarTimeout)
+  progressBarTimeout = setTimeout(NProgress.start, 500)
+}
+function stopProgressBar() {
+  clearTimeout(progressBarTimeout)
+  NProgress.done()
+}
 
 const routes = [
   {
@@ -19,3 +31,20 @@ const router = new VueRouter({
 })
 
 export default router
+
+router.beforeEach(async (to, from, next) => {
+  startProgressBar()
+  const { preload } = to.meta
+  if (preload) {
+    try {
+      await preload(next, to, from)
+    } catch (err) {
+      stopProgressBar()
+    }
+  } else {
+    next()
+  }
+})
+router.afterEach(() => {
+  stopProgressBar()
+})
